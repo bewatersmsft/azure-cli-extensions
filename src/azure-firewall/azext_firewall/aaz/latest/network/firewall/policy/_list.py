2f821e456fd17c761ab5629cbbcc2e08b0470400
Ben Eshed <105308016+bewatersmsft@users.noreply.github.com>
2024-10-24 17:14:01 -0700

    Revert "Azure Firewall IDPS new parameter - IDPS Profile (#7856)"
    This reverts commit e416e844abdba583e79db875447c7ccee5ef1dc0.

--- src/azure-firewall/azext_firewall/aaz/latest/network/firewall/policy/_list.py
+++ src/azure-firewall/azext_firewall/aaz/latest/network/firewall/policy/_list.py
@@ -316,7 +316,6 @@ class List(AAZCommand):
             intrusion_detection = cls._schema_on_200.value.Element.properties.intrusion_detection
             intrusion_detection.configuration = AAZObjectType()
             intrusion_detection.mode = AAZStrType()
-            intrusion_detection.profile = AAZStrType()
 
             configuration = cls._schema_on_200.value.Element.properties.intrusion_detection.configuration
             configuration.bypass_traffic_settings = AAZListType(
@@ -671,7 +670,6 @@ class List(AAZCommand):
             intrusion_detection = cls._schema_on_200.value.Element.properties.intrusion_detection
             intrusion_detection.configuration = AAZObjectType()
             intrusion_detection.mode = AAZStrType()
-            intrusion_detection.profile = AAZStrType()
 
             configuration = cls._schema_on_200.value.Element.properties.intrusion_detection.configuration
             configuration.bypass_traffic_settings = AAZListType(
